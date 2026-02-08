4f13baa15ce09af7b78ed90cb5ae028a6f7d0a47
Ali Hussein <joker1992992992@gmail.com>
2026-02-08 15:24:14 +0100

    vercel error

--- app/(app)/(admin)/dashboard/account/page.tsx
+++ app/(app)/(admin)/dashboard/account/page.tsx
@@ -387,7 +387,7 @@ export default function AccountPage() {
                     cy="50%"
                     labelLine={false}
                     label={({ name, percent }) =>
-                      `${name ?? "Unknown"} ${(percent * 100).toFixed(0)}%`
+                      `${name ?? "Unknown"} ${((percent ?? 0) * 100).toFixed(0)}%`
                     }
                     outerRadius={80}
                     fill="#8884d8"
