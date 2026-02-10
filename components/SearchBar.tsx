bb76a14bca3eb0183c98f74b460718bff6c8f2a2
Ali Hussein <joker1992992992@gmail.com>
2026-02-10 18:17:49 +0100

    search bar fix

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,6 +5,7 @@ import { Loader2, Search, X } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
+import { createPortal } from "react-dom";
 
 const SearchBar = ({ isScrolled }: { isScrolled: boolean }) => {
   const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
@@ -12,6 +13,11 @@ const SearchBar = ({ isScrolled }: { isScrolled: boolean }) => {
   const [resolts, setResolts] = useState<SearchItem[]>([]);
   const [loading, setLoading] = useState(false);
   const [open, setOpen] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
   useEffect(() => {
     const handler = setTimeout(() => {
@@ -28,8 +34,9 @@ const SearchBar = ({ isScrolled }: { isScrolled: boolean }) => {
     let cancelled = false;
     async function run() {
       const term = debouncedSearchTerm;
-      if (term.length < 0) {
+      if (term.trim().length === 0) {
         setResolts([]);
+        setLoading(false);
         return;
       }
 
@@ -99,92 +106,97 @@ const SearchBar = ({ isScrolled }: { isScrolled: boolean }) => {
         />
       </button>
       {/* modal search */}
-      {open && (
-        <div className="fixed inset-0 z-[60] bg-black/30 overflow-hidden">
-          {/* Center container */}
-          <div className="relative z-[70] flex min-h-screen items-start justify-center p-4 md:p-10">
-            {/* Modal panel */}
-            <div
-              ref={panelRef}
-              className="flex flex-col w-full max-w-2xl rounded-2xl bg-white p-6 shadow-lg overflow-hidden max-h-[80vh]"
-            >
-              {/* Header */}
-              <div className="flex items-center gap-3">
-                <form className="flex-1">
-                  <div className="relative">
-                    <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2" />
-                    <input
-                      value={searchInput}
-                      onChange={(e) => setSearchInput(e.target.value)}
-                      type="search"
-                      placeholder="Search for products..."
-                      className="w-full rounded-xl border border-lightColor bg-white py-2 pl-9 pr-3 outline-none"
-                    />
-                  </div>
-                </form>
+      {isMounted &&
+        open &&
+        createPortal(
+          <div className="fixed inset-0 z-[100] bg-black/30 overflow-hidden">
+            {/* Center container */}
+            <div className="relative z-[110] flex min-h-screen items-start justify-center p-4 md:p-10">
+              {/* Modal panel */}
+              <div
+                ref={panelRef}
+                className="flex flex-col w-full max-w-2xl rounded-2xl bg-white p-6 shadow-lg overflow-hidden max-h-[80vh]"
+              >
+                {/* Header */}
+                <div className="flex items-center gap-3">
+                  <form className="flex-1">
+                    <div className="relative">
+                      <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2" />
+                      <input
+                        value={searchInput}
+                        onChange={(e) => setSearchInput(e.target.value)}
+                        type="search"
+                        placeholder="Search for products..."
+                        className="w-full rounded-xl border border-lightColor bg-white py-2 pl-9 pr-3 outline-none"
+                      />
+                    </div>
+                  </form>
 
-                <button type="button" onClick={() => setOpen(false)}>
-                  <X className="h-6 w-6" />
-                </button>
-              </div>
+                  <button type="button" onClick={() => setOpen(false)}>
+                    <X className="h-6 w-6" />
+                  </button>
+                </div>
 
-              {/* Body */}
-              <div className="mt-4 flex-1 overflow-y-auto overscroll-contain scrollbar-hide">
-                {debouncedSearchTerm.trim().length === 0 ? (
-                  <p className="text-2xl font-medium space-y-10">
-                    Type to search
-                  </p>
-                ) : loading ? (
-                  <div className="flex items-center gap-2">
-                    <Loader2
-                      size={20}
-                      className="animate-spin text-shop_dark_yellow"
-                    />
-                    <p className="text-darkColor font-medium">Searching...</p>
-                  </div>
-                ) : (
-                  <ul className="space-y-2">
-                    {resolts.map((item) => (
-                      <li key={item._id}>
-                        <Link
-                          href={`/product/${item.slug}`}
-                          className="flex items-center justify-between gap-3 rounded-xl p-2 hover:bg-black/5"
-                          onClick={() => setOpen(false)}
-                        >
-                          <div className="flex items-center gap-3">
-                            <div className="h-10 w-10 overflow-hidden rounded-lg bg-black/5">
-                              {item.image ? (
-                                <Image
-                                  src={item.image}
-                                  alt="product-image"
-                                  width={40}
-                                  height={40}
-                                  className="h-10 w-10 object-cover"
-                                />
-                              ) : null}
-                            </div>
-
-                            <div>
-                              <p className="text-sm font-medium">{item.name}</p>
-                              <p className="text-xs opacity-70">
-                                {item.brand}
-                                {item.categories?.length ? (
-                                  <span> • {item.categories.join(", ")}</span>
+                {/* Body */}
+                <div className="mt-4 flex-1 overflow-y-auto overscroll-contain scrollbar-hide">
+                  {debouncedSearchTerm.trim().length === 0 ? (
+                    <p className="text-2xl font-medium space-y-10">
+                      Type to search
+                    </p>
+                  ) : loading ? (
+                    <div className="flex items-center gap-2">
+                      <Loader2
+                        size={20}
+                        className="animate-spin text-shop_dark_yellow"
+                      />
+                      <p className="text-darkColor font-medium">Searching...</p>
+                    </div>
+                  ) : (
+                    <ul className="space-y-2">
+                      {resolts.map((item) => (
+                        <li key={item._id}>
+                          <Link
+                            href={`/product/${item.slug}`}
+                            className="flex items-center justify-between gap-3 rounded-xl p-2 hover:bg-black/5"
+                            onClick={() => setOpen(false)}
+                          >
+                            <div className="flex items-center gap-3">
+                              <div className="h-10 w-10 overflow-hidden rounded-lg bg-black/5">
+                                {item.image ? (
+                                  <Image
+                                    src={item.image}
+                                    alt="product-image"
+                                    width={40}
+                                    height={40}
+                                    className="h-10 w-10 object-cover"
+                                  />
                                 ) : null}
-                              </p>
+                              </div>
+
+                              <div>
+                                <p className="text-sm font-medium">
+                                  {item.name}
+                                </p>
+                                <p className="text-xs opacity-70">
+                                  {item.brand}
+                                  {item.categories?.length ? (
+                                    <span> • {item.categories.join(", ")}</span>
+                                  ) : null}
+                                </p>
+                              </div>
                             </div>
-                          </div>
-                          <p className="text-sm font-medium">{item.price}$</p>
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                )}
+                            <p className="text-sm font-medium">{item.price}$</p>
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-      )}
+          </div>,
+          document.body
+        )}
     </>
   );
 };
